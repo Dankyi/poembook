bbf3b3fac327b6abe8a47c5331ba0f5eb75bdf4d
Dankyi <u1669769@unimail.hud.ac.uk>
2021-01-03 21:05:17 +0000

    Addition of google socialite feature

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        try
+        {
+            $user = User::query()
+                ->where('email', $googleUser->getEmail())
+                ->where('google_id', $googleUser->getId())
+                ->firstOrFail();
+        }
+        catch (ModelNotFoundException $exception)
+        {
+            try
+            {
+                $user = User::query()->where('email', $googleUser->getEmail())->firstOrFail();
+                $user->update(['google_id' => $googleUser->getId()]);
+            }
+            catch (ModelNotFoundException $exception)
+            {
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => Hash::make(Str::random(32)),
+                    'email_verified_at' => now(),
+                    'google_id' => $googleUser->getId()
+                ]);
+            }
+        }
+
+        Auth::login($user);
+
+        return redirect()->route('index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\GithubController;
+use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PoemController;
@@ -17,6 +18,9 @@ use App\Http\Controllers\PoemController;
 */
 
 Auth::routes();
+
+Route::get('/auth/google/redirect', [GoogleController::class, 'redirect'])->name('google.redirect');
+Route::get('/auth/google/callback', [GoogleController::class, 'callback'])->name('google.callback');
 
 Route::get('/auth/github/redirect', [GithubController::class, 'redirect'])->name('github.redirect');
 Route::get('/auth/github/callback', [GithubController::class, 'callback'])->name('github.callback');
